c345c3f08ca1670f04f9070d36a410ce7f89477f
Ge6ben <dl22gesh40@gmail.com>
2023-07-14 12:12:10 +0300

    init:initialize project

---
+++ src/layouts/dashboard/TheDefault.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Admin Dashboard default page</h1>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/dashboard/TheDefault.vue'),
+    children: [],
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+})
+
+export default router
