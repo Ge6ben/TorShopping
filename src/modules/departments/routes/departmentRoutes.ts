c65644accc2303f7cf8e8905a0bfc2e2e78b5fa9
Ge6ben <dl22gesh40@gmail.com>
2023-07-15 23:40:55 +0300

    feat:add department module basic

---
+++ src/modules/departments/routes/departmentRoutes.ts
@@ -0,0 +1,19 @@
+import { type RouteRecordRaw as Route } from 'vue-router'
+import { Layout } from '@/layouts/types/types'
+
+export const departmentRoutes: Route[] = [
+	{
+		path: '/departments',
+		name: 'departments',
+		component: () =>
+			import(
+				/* webpackChunkName: "DepartmentsChunk" */ '../views/Departments.vue'
+			),
+		meta: {
+			layout: Layout.ADMIN,
+			title: 'Departments'
+		}
+	}
+]
+
+export default departmentRoutes
---
+++ src/modules/departments/views/Departments.vue
@@ -0,0 +1,5 @@
+<template>
+	<div>Department!</div>
+</template>
+
+<script lang="ts" setup></script>
