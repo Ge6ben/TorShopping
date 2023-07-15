503e7a1955c927e0e9747926d846f1d7c1fec65b
Ge6ben <dl22gesh40@gmail.com>
2023-07-15 18:01:17 +0300

    feat:create Notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useNotification } from '@/composables/Notification'
+
+const { notification } = useNotification()
+</script>
+
+<template>
+	<v-snackbar
+		v-model="notification.open"
+		:color="notification.color"
+		:location="notification.location"
+		:timeout="notification.time"
+		style="z-index: 1007"
+	>
+		{{ notification.message }}
+	</v-snackbar>
+</template>
