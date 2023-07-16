3076fdc81b3cfaaace0d7aec0083f1a14f52bfa9
Ge6ben <dl22gesh40@gmail.com>
2023-07-16 11:10:04 +0300

    feat:add logout to navigation

--- src/layouts/views/Admin.vue
+++ src/layouts/views/Admin.vue
@@ -1,5 +1,7 @@
 <template>
+	<!--  Page Header   -->
 	<BaseHeader v-if="!currentRouteMeta.hidePageHeader" />
+	<!--  Side Bar   -->
 	<v-navigation-drawer
 		v-if="!currentRouteMeta.hideNavigation"
 		permanent
@@ -27,18 +29,79 @@
 			>
 			</v-list-item>
 		</v-list>
+
+		<template v-slot:append>
+			<BaseButton
+				append-icon="mdi-logout-variant"
+				block
+				color="black"
+				size="large"
+				variant="text"
+				@click="canOpenDialog = true"
+				>Log Out
+			</BaseButton>
+		</template>
 	</v-navigation-drawer>
+
+	<!-- Logout Dialog   -->
+	<BaseDialog
+		v-model="canOpenDialog"
+		:ok-button-props="{ loading }"
+		content="Are you sure do you want to logout?"
+		title="Logout"
+		@submit="handleLogout"
+	></BaseDialog>
+	<!-- @slot for Rest Content   -->
 	<slot />
 </template>
 
 <script lang="ts" setup>
 import { useRouteInfo } from '@/composables/currentRouterInfo'
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, ref } from 'vue'
 import { useNavigationItems } from '@/stores/navigationStore'
+import BaseButton from '@/components/BaseButton.vue'
+import BaseDialog from '@/components/BaseDialog.vue'
+
+import AxiosInstance from '@/plugins/axios'
+import router from '@/router'
+import { useNotification } from '@/composables/Notification'
+
+const { show: showSnackbar } = useNotification()
 
 const { getCurrentNavigation } = useNavigationItems()
 const BaseHeader = defineAsyncComponent(
 	() => import('../../components/BaseHeader.vue')
 )
 const { currentRouteMeta } = useRouteInfo()
+
+const loading = ref(false)
+const canOpenDialog = ref(false)
+
+/**
+ * The Logout allows users to securely log out of the application
+ */
+function handleLogout() {
+	loading.value = true
+	AxiosInstance.post(
+		'/api/auth/logout',
+		{},
+		{
+			headers: {
+				Authorization: 'Bearer 233|2gIxNsm3m89x00J2J64mCeCaTIu629XC06SA4dJz'
+			}
+		}
+	)
+		.then(() => {
+			showSnackbar('Logout successfully')
+			router.push('/login')
+		})
+		.catch(err => {
+			// TODO: Show backend error msg
+			showSnackbar(err.response.message, 'error')
+		})
+
+		.finally(() => {
+			loading.value = false
+		})
+}
 </script>
